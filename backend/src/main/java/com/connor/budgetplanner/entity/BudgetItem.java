42644b9ac98fa6c1eb191de0c0a802230c9cdbd4
Connor Evans <connor.l.evans.mil@army.mil>
2025-04-22 15:06:02 -0500

    added BudgetItem entity with fields and no-arg constructor

--- backend/src/main/java/com/connor/budgetplanner/entity/BudgetItem.java
+++ backend/src/main/java/com/connor/budgetplanner/entity/BudgetItem.java
@@ -1,4 +1,71 @@
 package com.connor.budgetplanner.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.Instant;
+
+@Entity
 public class BudgetItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private double amount;
+    private String category;
+    private Instant date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public BudgetItem(){
+
+    }
+
+    public BudgetItem(String description, double amount, String category, Instant date) {
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+    }
 }
